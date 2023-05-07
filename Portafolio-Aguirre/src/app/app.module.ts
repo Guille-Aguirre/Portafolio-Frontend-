a21a374a2a6cebecdb616cda9ac444a601b6cdf0
Guille-Aguirre <guillermodavidaguirre@gmail.com>
2023-05-07 01:38:52 -0300

    Se agrega hero banner

--- Portafolio-Aguirre/src/app/app.module.ts
+++ Portafolio-Aguirre/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { HeroComponent } from './Components/hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoApComponent
+    LogoApComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
