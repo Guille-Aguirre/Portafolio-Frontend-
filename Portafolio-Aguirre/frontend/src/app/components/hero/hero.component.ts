5c4d830ee6d23665a58f0016beaaa4230e227b31
Guille-Aguirre <guillermodavidaguirre@gmail.com>
2023-05-18 19:07:00 -0300

    implemteación del front al backend
    el objetivo es poder modificar el acerca de por medio del backend

--- Portafolio-Aguirre/frontend/src/app/components/hero/hero.component.ts
+++ Portafolio-Aguirre/frontend/src/app/components/hero/hero.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { persona } from 'src/app/model/persona.model';
+import { PersonaService } from 'src/app/serve/persona.service';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css']
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit{
+  persona: persona = new persona("","","");
 
+  constructor(public personaService:PersonaService){}
+  
+  ngOnInit(): void {
+    this.personaService.getPersona().subscribe(data=>{this.persona=data})
+  }
 }
